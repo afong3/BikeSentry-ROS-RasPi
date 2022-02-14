6702c334eb3833244822b77846292672076ea1df
Adam Fong <adamfong888@gmail.com>
2022-02-14 09:21:03 -0800

    Changing audio_recorder
    Sounddevice works like a charm. Not sure why there isn't much public documentation

--- src/audio_recorder.py
+++ src/audio_recorder.py
@@ -8,58 +8,34 @@ import rospy
 from std_msgs.msg import String
 import os
 import wave
-import pyaudio
+import sounddevice as sd
 import time
 import math
 import rospkg
+import scipy.io.wavfile as wav
 
 
 rospack = rospkg.RosPack()
 pck_path = rospack.get_path('bike_sentry_raspi')
 
-def record_audio(seconds, filename):
+def record_audio(duration, filename):
     '''
-    Record a .wav file for a defined amount of seconds. Output to a user defined filename.
+    Record a .wav file for a defined amount of duration. Output to a user defined filename.
     Return: string that has filename without file extension 
     '''
-    form_1 = pyaudio.paFloat32 # 16-bit resolution
-    chans = 1 # 1 channel
-    samp_rate = 44100 # 44.1kHz sampling rate
-    chunk = 4096 # 2^12 samples for buffer
-    record_secs = seconds # seconds to record
-    dev_index = 2 # device index found by p.get_device_info_by_index(ii)
-    recordings_directory = pck_path + '/recordings/'
-    wav_output_filename = recordings_directory + filename + ".wav" # name of .wav file
+    f = filename.data
+    full_path = pck_path + "/" + f + ".wav"
+    fs = 44100
+    sd.default.samplerate = fs
+    sd.default.channels = 1
 
-    audio = pyaudio.PyAudio() # create pyaudio instantiation
+    rec = sd.rec(int(duration * fs))
+    sd.wait()
 
-    # create pyaudio stream
-    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
-                        input_device_index = dev_index,input = True, \
-                        frames_per_buffer=chunk)
-    frames = []
+    # save .wav
+    wav.write(full_path, fs, rec)
 
-    # loop through stream and append audio chunks to frame array
-    for ii in range(0,int((samp_rate/chunk)*record_secs)):
-        data = stream.read(chunk)
-        frames.append(data)
-
-    # stop the stream, close it, and terminate the pyaudio instantiation
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    # save the audio frames as .wav file
-    wavefile = wave.open(wav_output_filename,'wb')
-    wavefile.setnchannels(chans)
-    wavefile.setsampwidth(audio.get_sample_size(form_1))
-    wavefile.setframerate(samp_rate)
-    wavefile.writeframes(b''.join(frames))
-    wavefile.close()
-
-    rospy.loginfo(("{} saved".format(filename)))
-    
-    return filename
+    return f
 
 def main():
     rospy.init_node("audio_recorder")
