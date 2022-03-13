27ef95828f371c34e5c69d86da3966b3a2bef133
erikd234 <edahl@deltacontrols.com>
2022-03-12 18:12:03 -0800

    Adding theft alert

---
+++ scripts/theft_alert.py
@@ -0,0 +1,13 @@
+
+
+
+def main():
+    print("hello")
+    url = r"https://bike-sentry-api-2vgam74tba-uc.a.run.app/theft_alert/T0"
+    res = requests.post(url)
+    print(res)
+
+
+
+if __name__ == "__main__":
+    main()
